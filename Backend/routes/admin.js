b79bfda7c1709bb25842842120199fa31416b344
tubs-hust <tubui7121@gmail.com>
2019-04-09 13:44:42 +0700

    custom config

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -2,34 +2,28 @@ var express = require('express');
 var router = express.Router();
 const user_md = require('../models/user');
 
+router.get('/', function (req, res, next) {
+    res.render("admin/index")
+})
+
 /* GET home page. */
 router.get('/allusers', function (req, res, next) {
-    let data = {};
     user_md.getAllUsers()
         .then(users => {
-            res.json({ data: users });
+            res.json(users);
         })
 });
 
 router.get('/user/:id', function (req, res, next) {
     let id = req.params.id;
 
+    console.log(id);
+
     user_md.getUserById(id)
         .then(users => {
-            data = users[0];
-            console.log(data.a_row);
-            res.json({ data: data })
+            res.json(users);
         })
 })
 
-router.get('role/:id', function (req, res, next) {
-    let id = req.params.id;
-
-    user_md.getRoleById(id)
-        .then(users => {
-            data.role = users[0];
-            res.json({ data: data })
-        })
-})
 
 module.exports = router;
