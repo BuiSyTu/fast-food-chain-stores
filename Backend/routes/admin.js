6a08c47cc12e0cb216fb0d41eef657c5a80fcf7b
tubs-hust <tubui7121@gmail.com>
2019-04-15 16:16:52 +0700

    fix conflict admin.js

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -3,28 +3,42 @@ var router = express.Router();
 const user_md = require('../models/user');
 const bill_md = require('../models/bill');
 
-router.get('/', function (req, res, next) {
-    res.render("admin/index")
-})
-
 /* GET home page. */
 router.get('/allusers', function (req, res, next) {
+    let data = {};
     user_md.getAllUsers()
         .then(users => {
-            res.json(users);
+            res.json({
+                data: users
+            });
         })
 });
 
 router.get('/user/:id', function (req, res, next) {
     let id = req.params.id;
 
-    console.log(id);
-
     user_md.getUserById(id)
         .then(users => {
-            res.json(users);
+            data = users[0];
+            console.log(data.a_row);
+            res.json({
+                data: data
+            })
         })
 })
+
+router.get('role/:id', function (req, res, next) {
+    let id = req.params.id;
+
+    user_md.getRoleById(id)
+        .then(users => {
+            data.role = users[0];
+            res.json({
+                data: data
+            })
+        })
+})
+
 
 // Hoa don
 
@@ -50,6 +64,10 @@ router.get('/bill/:id', (req, res, next) => {
                 data: data
             });
         })
+})
+
+router.get('/', function (req, res, next) {
+    res.render("admin/index")
 })
 
 module.exports = router;
